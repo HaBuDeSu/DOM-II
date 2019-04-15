9bbf88b41b7320132b2f45dc8f6b1528d2ac33bf
Ken Ruf <ruf.kenichi@gmail.com>
2019-04-14 20:57:16 -0500

    added mouseover event for navItems and scroll event for header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
-const navItems = document.querySelectorAll("nav a");
-const header = document.querySelctor(".main-navigation");
+const navItems = document.querySelectorAll(".nav-link");
+const header = document.querySelector(".main-navigation");
 const logoHeading = document.querySelector(".logo-heading");
 const introImage  = document.querySelector(".intro img");
 const imageContent = document.querySelector(".img-content img");
@@ -10,13 +10,13 @@ const body = document.querySelector("body");
 const destinationImage  = document.querySelector(".content-destination img");
 const text = document.querySelectorAll("p");
 
-// .addEventListener('mouseover', )
+navItems.forEach(element => element.addEventListener('mouseover', (event) => {event.target.style.color = 'red'}));
 // .addEventListener('keydown', )
 // .addEventListener('wheel', )
 // .addEventListener('drag / drop', )
 // .addEventListener('load', )
-// .addEventListener('focus', )
+// .addEventListener('click', )
 // .addEventListener('resize', )
-// .addEventListener('scroll', )
+document.addEventListener('scroll', (event) => {header.style.backgroundColor = "yellow"});
 // .addEventListener('select', )
 // .addEventListener('dblclick', )
