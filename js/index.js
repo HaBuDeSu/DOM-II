99c6ec19e3c898840528eb19ea9532879f62e387
Ken Ruf <ruf.kenichi@gmail.com>
2019-04-14 20:39:44 -0500

    created element selectors and eventlistener templates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const navItems = document.querySelectorAll("nav a");
+const header = document.querySelctor(".main-navigation");
+const logoHeading = document.querySelector(".logo-heading");
+const introImage  = document.querySelector(".intro img");
+const imageContent = document.querySelector(".img-content img");
+const signupButton = document.querySelector("btn");
+const footer = document.querySelector("footer");
+const body = document.querySelector("body");
+const destinationImage  = document.querySelector(".content-destination img");
+const text = document.querySelectorAll("p");
+
+// .addEventListener('mouseover', )
+// .addEventListener('keydown', )
+// .addEventListener('wheel', )
+// .addEventListener('drag / drop', )
+// .addEventListener('load', )
+// .addEventListener('focus', )
+// .addEventListener('resize', )
+// .addEventListener('scroll', )
+// .addEventListener('select', )
+// .addEventListener('dblclick', )
