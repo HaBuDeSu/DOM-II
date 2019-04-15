78964fe64d21a6178e23c24990fa440ea0a7ef3c
Ken Ruf <ruf.kenichi@gmail.com>
2019-04-14 22:23:06 -0500

    added click event for contentPick and stopped propagation to child divs

--- js/index.js
+++ js/index.js
@@ -9,12 +9,16 @@ const footer = document.querySelector("footer");
 const body = document.querySelector("body");
 const destinationImage  = document.querySelector(".content-destination img");
 const text = document.querySelectorAll("p");
+const contentPick = document.querySelector(".content-pick");
+const contentPickDivs = document.querySelectorAll(".content-pick div");
+
 
 navItems.forEach(navItem => navItem.addEventListener('mouseover', (event) => {event.target.style.color = 'red'}));
 document.addEventListener('keydown', (event) => {logoHeading.textContent = logoHeading.textContent + event.key});
 document.addEventListener('wheel', (event) => {body.style.backgroundColor = "grey"});
-// .addEventListener('load', )
+contentPick.addEventListener('click', (event) => {event.target.style.backgroundColor = "red"});
 signupButton.forEach(button => button.addEventListener('click', (event) => event.target.textContent = "Are you sure?"));
+contentPickDivs.forEach(div => div.addEventListener('click', (event) => {event.stopPropagation()}));
 // .addEventListener('resize', )
 document.addEventListener('scroll', (event) => {header.style.backgroundColor = "yellow"});
 // .addEventListener('select', )
