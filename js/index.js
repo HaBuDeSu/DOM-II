c5c0fb3cf31bd0c5fb65285f9dfa44c9668d048d
Ken Ruf <ruf.kenichi@gmail.com>
2019-04-14 21:47:30 -0500

    added click event for signupButton and dblclick event for text

--- js/index.js
+++ js/index.js
@@ -3,20 +3,19 @@ const navItems = document.querySelectorAll(".nav-link");
 const header = document.querySelector(".main-navigation");
 const logoHeading = document.querySelector(".logo-heading");
 const introImage  = document.querySelector(".intro img");
-const imageContent = document.querySelector(".img-content img");
-const signupButton = document.querySelector("btn");
+const imageContent = document.querySelectorAll(".img-content img");
+const signupButton = document.querySelectorAll(".btn");
 const footer = document.querySelector("footer");
 const body = document.querySelector("body");
 const destinationImage  = document.querySelector(".content-destination img");
 const text = document.querySelectorAll("p");
 
-navItems.forEach(element => element.addEventListener('mouseover', (event) => {event.target.style.color = 'red'}));
+navItems.forEach(navItem => navItem.addEventListener('mouseover', (event) => {event.target.style.color = 'red'}));
 // .addEventListener('keydown', )
 // .addEventListener('wheel', )
-// .addEventListener('drag / drop', )
 // .addEventListener('load', )
-// .addEventListener('click', )
+signupButton.forEach(button => button.addEventListener('click', (event) => event.target.textContent = "Are you sure?"));
 // .addEventListener('resize', )
 document.addEventListener('scroll', (event) => {header.style.backgroundColor = "yellow"});
 // .addEventListener('select', )
-// .addEventListener('dblclick', )
+text.forEach(paragraph => paragraph.addEventListener('dblclick', (event) => event.target.style.fontFamily = 'Indie Flower'));
